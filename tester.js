dc7467629c4e671cc5894966850b0df35e7da602
joe0400 <joe0400@gmail.com>
2021-09-09 12:14:34 -0400

    lol pls fix

--- tester.js
+++ tester.js
@@ -47,7 +47,7 @@ var peer = new Peer()
             text: "https://joe0400.github.io?host=true&id="+id,
             width:256,
             height:256,
-            correctLevel : QRCode.CorrectLevel.Q
+            correctLevel:QRCode.CorrectLevel.H
         });
         console.log("file:///home/joe0400/sendit/receive.html?id="+id);
         
